351984f632566c7d5f167665ae92580999f0a8f4
Arjun Saini <69866165+Arjun-Saini@users.noreply.github.com>
2026-05-05 20:00:40 -0400

    revert network wait

--- start_client.sh
+++ start_client.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-cd /home/pi/pikart
+cd /home/pi/PiKart
+sleep 15
 pigpiod
 sleep 2
-
-until ping -c1 -W1 192.168.50.1 > /dev/null 2>&1; do
-    echo "Waiting for host..."
-    sleep 1
-done
-
 python /home/pi/PiKart/pikart_client.py
--- start_host.sh
+++ start_host.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 cd /home/pi/PiKart
+sleep 10
 pigpiod
 sleep 2
-
-until ip addr show wlan0 | grep -q '192.168.50.1'; do
-    echo "Waiting for wlan0..."
-    sleep 1
-done
-
 python /home/pi/PiKart/pikart_host.py
